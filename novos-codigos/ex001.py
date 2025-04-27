db86b462666ebfe9c7da0efa7cf0c126b8da1129
DenisVidal06 <crfdenis0606@hotmail.com>
2025-04-27 16:14:11 -0400

    adicionado uma solução

--- novos-codigos/ex001.py
+++ novos-codigos/ex001.py
@@ -1,1 +1,11 @@
 # nesse algoritimo, crie uma variavel que armazene uma string e uma lista que armazene varias strings.
+
+# Criando uma variável que armazena uma string
+minha_string = "Olá, mundo!"
+
+# Criando uma lista que armazena várias strings
+minha_lista = ["Python", "JavaScript", "C++", "Java"]
+
+# Exibindo os valores
+print(minha_string)
+print(minha_lista)
